91fbf8a5cba6946bc3c3c27b42ad4206ec078739
LBJ <lbjcena@gmail.com>
2024-02-08 21:06:14 +0100

    error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,14 @@ export async function createInvoice (formData: FormData) {
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
 
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+  } catch {
+    alert('Shit happened')
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
@@ -42,17 +46,24 @@ export async function updateInvoice (id: string, formData: FormData) {
   })
 
   const amountInCents = amount * 100
+  try {
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `
+  } catch {
+    alert('Shit happened')
+  }
 
-  await sql`
-  UPDATE invoices
-  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-  WHERE id = ${id}
-  `
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
-
 export async function deleteInvoice (id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`
-  revalidatePath('/dashboard/invoices')
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`
+    revalidatePath('/dashboard/invoices')
+  } catch {
+    alert('Shit happened')
+  }
 }
